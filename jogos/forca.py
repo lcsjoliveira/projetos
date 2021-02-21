2b572a2891c04a2787fe0fb336becf4f12d94433
root <root@DESKTOP-HVTSCM8.localdomain>
2021-02-21 12:59:30 -0300

    develop: Adicionando novas funções para os jogos

---
+++ jogos/forca.py
@@ -0,0 +1,5 @@
+def jogar():
+    print('Bem vindo ao jogo da forca!')
+
+    if __name__ == 'main':
+        jogar()
---
+++ jogos/jogos.py
@@ -0,0 +1,13 @@
+import forca
+import adivinhacao
+
+print('Escolha o seu jogo')
+print('1 Forca 2 Adivinhação')
+
+jogo = int(input('Qual jogo?'))
+if jogo == 1:
+    print('Jogando forca')
+    forca.jogar()
+elif jogo == 2:
+    print('Jogando adivinhação!')
+    adivinhacao.jogar()
